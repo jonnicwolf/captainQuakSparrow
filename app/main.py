cf823b0fbd6abf47313b830256e4ca4931ba0f88
Jon Narine <jon.wolf.narine@gmail.com>
2025-07-02 19:15:46 -0400

    rftr: changes get to post and adds basic setup for listener

--- app/main.py
+++ app/main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
 
+import tempfile
+
 app = FastAPI()
 
-@app.get("/")
-def root():
-  return {"Hello": "World"}
+@app.post("/listen")
+async def listen(audio: UploadFile = File(...)):
+  with tempfile.NamedTemporaryFile(delete=False, suffix=".wav") as tmp:
+    tmp.write(await audio.read())
+    tmp_path = tmp.name
+    return "yo"
